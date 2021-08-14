70c895c08098e0ff6df142bb5a966ba6c85bf454
HN <kazukiyoshida96@gmail.com>
2021-08-13 19:26:45 -0700

    RENAME app.py -> mastodon.py

--- mastodon.py
+++ mastodon.py
@@ -4,6 +4,10 @@ import requests
 
 MSTDNAPI_URL = "https://mastodon.cloud/api/v1/"
 
+CLIENT_KEY = "bpwlCoRzPkSykU017HyKKPhoNVVOfbICKUqcLj-RXwo"
+CLIENT_SECRET = "vfDk70s4-BP0mSNJm63Qpo3MJLLQNRei8ZuW4YrRCqg"
+ACCESS_TOKEN = "DoGN-q3Sx4Z6WGdZmsgY5Iks7dF7pKAv2NpjtNI1ubQ"
+
 
 def publish_new_status(access_token: str, status: str, media_ids: list = None, poll: dict = None) -> dict:
 
