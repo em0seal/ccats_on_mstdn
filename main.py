246e9367f0d2b2aa5d7646cd417fc41a9a1e1ad9
HN <kazukiyoshida96@gmail.com>
2021-08-14 16:29:04 -0700

    ADD some comments about crontab

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ from mastodon import publish_status_with_media
 
 
 if __name__ == "__main__":
-    
+
     btc_datafile = "btc2usd.csv"
     img_file = "btc_analysis.png"
     # Please set the access_token of mastodon in environment variables
+    # In AWS EC2 instance: Amazon Linux 2 AMI, cannot use os.environ
+    # So, for now, directly set access_token in EC2 instance
     token_env = "MASTODON_CLOUD_TOKEN"
 
     print("="*80)
@@ -21,7 +23,6 @@ if __name__ == "__main__":
     # generate or update btc data csv file
     get_data(*btc_datafile.split("."))
     print(f"[Finished fetching bitcoin data] -> {btc_datafile}")
-
 
     date_today = str(datetime.datetime.now(tz=pytz.utc).date())
     date_90days_ago = str((datetime.datetime.now(tz=pytz.utc) - datetime.timedelta(days=90)).date())
@@ -36,4 +37,4 @@ if __name__ == "__main__":
     # publish the image to mastodon
     status = f"[bot] Bitcoin Chart Today\n(=^･ω･^=)<Have a good one!\n\n{datetime.datetime.now()}"
     publish_status_with_media(access_token=os.environ[token_env], status=status, filename=img_file)
-    print(f"[Finished publising the image to Mastodon]")
+    print("[Finished publising the image to Mastodon]")
